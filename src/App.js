d5765a52bdaf24de51df45588d32a64f5e959744
Jake Filipowski <filipowskijm@gmail.com>
2023-12-22 15:19:32 -0500

    adding background

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import TopBar from './components/TopBar';
+import Background from './components/Background';
 
 function App() {
   return (
     <div className="App">
       <TopBar></TopBar>
+      <Background></Background>
       <h1>Hello World</h1>
     </div>
   );
